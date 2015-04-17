7183c487b1cb026003e5af78d61b54dcf5b7c2ad
Daniel Pham <pham.dany@gmail.com>
2015-04-17 09:59:49 -0400

    Run go fmt on source code

--- main.go
+++ main.go
@@ -1,54 +1,54 @@
 package sql2json
 
 import (
-    "strings"
-    "encoding/json"
-    "database/sql"
+	"database/sql"
+	"encoding/json"
+	"strings"
 )
 
 func Run(rows *sql.Rows) (string, error) {
-    columns, err := rows.Columns()
-    if err != nil {
-        return "", err
-    }
+	columns, err := rows.Columns()
+	if err != nil {
+		return "", err
+	}
 
-    count := len(columns)
+	count := len(columns)
 
-    tableData := make([]map[string]interface{}, 0)
-    values := make([]interface{}, count)
-    valuePtrs := make([]interface{}, count)
+	tableData := make([]map[string]interface{}, 0)
+	values := make([]interface{}, count)
+	valuePtrs := make([]interface{}, count)
 
-    for i, _ := range columns {
-        valuePtrs[i] = &values[i]
-    }
+	for i, _ := range columns {
+		valuePtrs[i] = &values[i]
+	}
 
-    for rows.Next() {
-        err := rows.Scan(valuePtrs...)
-        if err != nil {
-            return "", err
-        }
+	for rows.Next() {
+		err := rows.Scan(valuePtrs...)
+		if err != nil {
+			return "", err
+		}
 
-        entry := make(map[string]interface{}, 0)
-        for i, key := range columns {
-            var v interface{}
-            rawValue := values[i]
+		entry := make(map[string]interface{}, 0)
+		for i, key := range columns {
+			var v interface{}
+			rawValue := values[i]
 
-            s, ok := rawValue.([]byte)
-            if ok {
-                v = strings.TrimSpace(string(s))
-            } else {
-                v = rawValue
-            }
+			s, ok := rawValue.([]byte)
+			if ok {
+				v = strings.TrimSpace(string(s))
+			} else {
+				v = rawValue
+			}
 
-            entry[key] = v
-        }
-        tableData = append(tableData, entry)
-    }
+			entry[key] = v
+		}
+		tableData = append(tableData, entry)
+	}
 
-    jsonData, err := json.Marshal(tableData)
-    if err != nil {
-        return "", err
-    }
+	jsonData, err := json.Marshal(tableData)
+	if err != nil {
+		return "", err
+	}
 
-    return string(jsonData), err
+	return string(jsonData), err
 }
